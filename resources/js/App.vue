bdf58033b8455ea288cc1ae27f30a431c618f944
Joe Bailey <joe@joebailey.xyz>
2022-06-29 14:46:30 +0100

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,36 @@
+<style scoped>
+</style>
+
+<template>
+  <div v-text="name" />
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+
+  mounted() {
+    this.getBasicInfo();
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    getBasicInfo() {
+      axios
+        .get("/api/basicinfo")
+        .then((response) => {
+          console.log(response.data);
+          this.name = response.data.name;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+        html {
+            line-height: 1.15;
+            -webkit-text-size-adjust: 100%
+        }
+
+        body {
+            margin: 0
+        }
+
+        a {
+            background-color: transparent
+        }
+
+        [hidden] {
+            display: none
+        }
+
+        html {
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+            line-height: 1.5
+        }
+
+        *,
+        :after,
+        :before {
+            box-sizing: border-box;
+            border: 0 solid #e2e8f0
+        }
+
+        a {
+            color: inherit;
+            text-decoration: inherit
+        }
+
+        svg,
+        video {
+            display: block;
+            vertical-align: middle
+        }
+
+        video {
+            max-width: 100%;
+            height: auto
+        }
+    </style>
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
